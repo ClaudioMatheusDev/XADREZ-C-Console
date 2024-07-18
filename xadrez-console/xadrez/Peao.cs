f1f8578e81d449336fa41ac4e69c5366521f5a88
Claudio Matheus <claudiomatheus055@gmail.com>
2024-07-18 15:22:45 -0300

    Jogada especial en passant

--- xadrez-console/xadrez/Peao.cs
+++ xadrez-console/xadrez/Peao.cs
@@ -4,8 +4,10 @@ using tabuleiro;
 namespace xadrez { 
     internal class Peao : Peca
 {
-    public Peao(Tabuleiro tab, Cor cor) : base(tab, cor)
+    private PartidaDeXadrez partida;    
+    public Peao(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
     {
+            this.partida = partida;
     }
 
     public override string ToString()
@@ -55,7 +57,25 @@ namespace xadrez {
                 mat[pos.linha, pos.coluna] = true;
             }
 
-        }//MOVIMENTO PEÃO COR BRANCO
+
+            // #jogadaespecial en passant
+            if (posicao.linha == 3)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigos(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                    {
+                        mat[esquerda.linha - 1, esquerda.coluna] = true;
+                    } //PEÇAS NA POSIÇÃO DA ESQUERDA
+
+                    Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigos(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                    {
+                        mat[direita.linha - 1, direita.coluna] = true;
+                    }//PEÇAS NA POSIÇÃO DA ESQUERDA
+
+                } // #jogadaespecial en passant
+
+            }//MOVIMENTO PEÃO COR BRANCO
 
         else
         {
@@ -84,8 +104,25 @@ namespace xadrez {
                 mat[pos.linha, pos.coluna] = true;
             }
 
+                // #jogadaespecial en passant
+                if (posicao.linha == 4)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigos(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                    {
+                        mat[esquerda.linha + 1, esquerda.coluna] = true;
+                    } //PEÇAS NA POSIÇÃO DA ESQUERDA
 
-        }//MOVIMENTO PEÃO COR PRETA
+                    Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigos(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                    {
+                        mat[direita.linha + 1, direita.coluna] = true;
+                    }//PEÇAS NA POSIÇÃO DA ESQUERDA
+
+                } // #jogadaespecial en passant
+
+
+            }//MOVIMENTO PEÃO COR PRETA
 
             return mat;
 
