14307f4178e4020f8055fd36ddecf4a08f6aefad
Claudio Matheus <claudiomatheus055@gmail.com>
2024-07-18 15:30:16 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -154,7 +154,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }//Não deixa o usuario se colocar em xeque
-            
+
+            Peca p = tab.peca(destino);
+
+            // #JOGADAESPECIAL PROMOCAO
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p .cor == Cor.Preta && destino.linha == 7)){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }// #JOGADAESPECIAL PROMOCAO
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -174,7 +189,6 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
             // #JOGADAESPECIAL EN PASSANT
 
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
